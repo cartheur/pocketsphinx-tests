43c9e585abbd2b9474536d53dd08ab877d186e9c
C.A.Tucker <cartheur@pm.me>
2024-10-30 21:09:28 +0100

    Add bits

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 ﻿
+using System.Diagnostics;
+
 public class Program
 {
+    // Pocketsphinx voice recognition system
+    public static bool SphinxUsed { get; set; }
+    public static bool PocketSphinxPythonUsed { get; set; }
+    public static Process PocketSphinxProcess { get; set; }
+    public static Process PocketSphinxPythonProcess { get; set; }
+    public static string PocketSphinxPythonProcessOutput { get; set; }
+    public static int PocketSphinxPythonProcessExitCode { get; set; }
+    public static string PocketsphinxFileDrop { get; set; }
+    public static StreamReader PythonProcessStream { get; set; }
+    public static bool AeonLoaded { get; set; }
     /// <summary>
     /// Initializes the speech recognizer.
     /// </summary>
@@ -13,6 +25,14 @@ public class Program
 
     static void Main(string[] args)
     {
+        PocketSphinxPythonUsed = Convert.ToBoolean(_thisAeon.GlobalSettings.GrabSetting("pocketsphinxused"));
+        AeonLoaded = true;
+        // Determine whether to load Windows speech synthesizer.
+        if (PocketSphinxPythonUsed && AeonLoaded)
+            InitializePocketSphinx();
+        //if (SapiWindowsUsed)
+            //InitializeSpeechRecognizer();
+
         Console.WriteLine("Hello, Worldly!");
     }
 }
